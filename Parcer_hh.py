e9b381cc259e0accedecec04a99a5fc0c4975e38
dodon4ik <miklindenis5552@gmail.com>
2019-04-24 17:32:18 +0300

    Parcer hh.ru

---
+++ Parcer_hh.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+headers={'accept':'*/*','user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+base_url='https://hh.ru/search/vacancy?search_period=3&area=1&text=Python&page=0'
+
+
+def hh_parce(base_url,headers):
+    session=requests.Session()
+    request=session.get(base_url,headers=headers)
+    if request.status_code==200:
+        soup=bs(request.content,'html.parser')
+        divs=soup.find_all('div',attrs={'data-qa':"vacancy-serp__vacancy"})
+        for div in divs:
+            title=div.find('a',attrs={'data-qa':"vacancy-serp__vacancy-title"}).text
+            href=div.find('a',attrs={'data-qa':"vacancy-serp__vacancy-title"})['href']
+            requement=div.find('div',attrs={'data-qa':"vacancy-serp__vacancy_snippet_requirement"}).text
+            print(title)
+            print(href)
+            print(requement)
+        
+    else:
+        print('AND WHY I NEED TO KNOW THIS INFORMATION?')
+
+hh_parce(base_url,headers)
